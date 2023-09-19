2d18838070beeab1c7658243abadbc9402d42c12
Alexandr <fresh.fin16@gmail.com>
2023-09-19 21:13:44 +0300

    js mod

--- js/app.js
+++ js/app.js
@@ -1,18 +1,28 @@
 "use strict";
 
-const obj = {
-    name: 'Ivan',
-    age: 18,
-    earning: {
-           cash: 1200
-    }
-}
+// const obj = {
+//     name: 'Ivan',
+//     age: 18,
+//     earning: {
+//            cash: 1200
+//     }
+// }
 
-obj.name = 'Alexey';
-console.log(obj.name);
+// obj.name = 'Alexey';
+// console.log(obj.name);
 
 
-const arr = ['Alex', 18, 'Chinnik']
-arr[3] = 'Love Mary';
-console.log(arr[3]);
+// const arr = ['Alex', 18, 'Chinnik']
+// arr[3] = 'Love Mary';
+// console.log(arr[3]);
 
+
+let name = 'Как вас зовут?',
+    age = 'Сколько вам лет?',
+    from = 'Откуда вы?';
+
+let result = prompt(`Ответьте на вопрос ${name}`, '');
+let result = prompt(`Ответьте на вопрос ${age}`, '');
+let result = prompt(`Ответьте на вопрос ${from}`, '');
+
+console.log(result);
