ebfa038348ac4a2e78f10654f6530de7e8d62d87
Alexandr <fresh.fin16@gmail.com>
2023-09-23 12:51:23 +0300

    learning function

--- js/app.js
+++ js/app.js
@@ -1,50 +1,44 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
+// const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
 
-const personalMoviesDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-}
+// const personalMoviesDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
 
- if (personalMoviesDB.count <= 10){
-    console.log('Просмотрено довольно мало фильмов')
-} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
-    console.log('Вы классический зритель')
-} else if (personalMoviesDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('error');
-    i--;
-}
-
-
-
-    
-
-
-    for(let i = 0; i < 2; i++){
-        const a = prompt ('Один из последних просмотренных фильмов?', ''),
-              b = prompt ('На сколько оцените его?', '');
+//  if (personalMoviesDB.count <= 10){
+//     console.log('Просмотрено довольно мало фильмов')
+// } else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+//     console.log('Вы классический зритель')
+// } else if (personalMoviesDB.count >= 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('error');
+//     i--;
+// }
+//     for(let i = 0; i < 2; i++){
+//         const a = prompt ('Один из последних просмотренных фильмов?', ''),
+//               b = prompt ('На сколько оцените его?', '');
 
 
-        if(a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50){
-        console.log('done');
-        personalMoviesDB[a] = b;
+//         if(a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50){
+//         console.log('done');
+//         personalMoviesDB[a] = b;
 
-     } else {
-        console.log('error');
-            i--;
-     }
-}
+//      } else {
+//         console.log('error');
+//             i--;
+//      }
+// }
 
 
 
 
 
 
-    console.log(personalMoviesDB);
+//     console.log(personalMoviesDB);
 
