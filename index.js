ee9f52fe2c2ee5812568c0a65dc6be274536bc93
Alexandr <fresh.fin16@gmail.com>
2023-09-24 16:31:12 +0300

    return test

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 'use strict';
 
-const usdCurs = 36.80;
-const eurCurs = 39.28;
-function convert (amount, curs) {
-    console.log(amount * curs);
-}
-convert(7000, usdCurs);
+// const usdCurs = 37;
+// const eurCurs = 39;
+// const discount = 0.9;
+
+// function convert (amount, curs) {
+//     return amount * curs;
+// }
+// function promotion (result) {
+//     console.log(result * discount)
+// }
+
+// const res = convert(100, usdCurs);
+// promotion(res);
+
+
+
+
+// function test () {
+//     for(let i = 0; i < 5; i++){
+//         console.log(i);
+//         if (i === 3) return;
+//     }
+//     console.log('Done');
+// }
+// test();
+
+
+// function doNothink (){};
+// console.log(doNothink() === undefined);
+
+
+
+
+
+ 
+
+
+
+
+
