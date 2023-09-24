086d9f2781e853ab73a3ed8466ce1e3afc15e334
Alexandr <fresh.fin16@gmail.com>
2023-09-24 13:50:06 +0300

    second try

--- index.js
+++ index.js
@@ -1,45 +1,8 @@
 'use strict';
-// const lines = 8;
-// let result = '';
 
-// for(let index = 0; index < lines; index++) {
-//     if (index == 0) {
-//         result += ' ';
-//     } 
-//     for(let key = 0; key < lines; key++){
-//         result += '#';
-       
-//         for(let step = 0; step < 1; step++){
-//             result += ' ';
-//         }
-//     }
-    
-//     result += ' ';
-  
-//     result += '\n';
-// }
-
-// let num = 10;
-
-// function showFirstMessage(name) {
-//    console.log(name);
-//     let num = 20;
-//     console.log(num)
-// }
-// showFirstMessage('Я люблю Машу');
-
-
-
-// function calc(a, b) {
-//     return(a * b); 
-// }
-// console.log(calc(1200, 1));
-// console.log(calc(23, 12));
-// console.log(calc(13, 124));
-
-
-function ret() {
-let num = 50
-return num;
+const usdCurs = 36.80;
+const eurCurs = 39.28;
+function convert (amount, curs) {
+    console.log(amount * curs);
 }
-console.log(ret())
+convert(7000, usdCurs);
