cd707f73269fd9c261c1564c6cf554e26f300ef6
Alexandr <fresh.fin16@gmail.com>
2023-09-26 13:46:26 +0300

    personal movie DB

--- index.js
+++ index.js
@@ -1,71 +1,16 @@
 "use strict";
-// 3) Создайте функцию, которая будет принимать в себя 2 аргумента, оба числа. 
-// Первое число - это база, второе число - это сколько раз нужно будет повторить это 
-// число в прогрессии. (Смотри пример ниже). Функция должна возвращать 
-// строку (или число в особых случаях, о которых ниже), где эти числа идут по порядку, 
-// разделенные тремя дефисами "---". После последнего числа их не должно быть.
 
-// Если второй аргумент не является числом, равен или меньше нуля - то возвращать просто 
-// первый аргумент. (Проверяем через оператор typeof)
+// const str = 'Some people want to do';
+// console.log(str.slice(5, 11));
+// console.log(str.indexOf('Some'));
+// console.log(str.substring(5, 11));
+// console.log(str.substr(5, 2));
 
 
-// function getMathResult(firstnum, secondnum) {
-//     if(typeof(secondnum) != 'number' || secondnum <= 0){
-//         return firstnum;
-//     }
-//     let str = '';
-//     for(let i = 1; i <= secondnum; i++){
-//         if(i === secondnum) {
-//             str += `${firstnum * i}`
+// const num = 12.5;
+// console.log(Math.round(num));
 
-//         } else {
-//             str += `${firstnum * i}---`
-//         }
-//     }
-//     return str;
-// }
+// const number = '12px';
+// // console.log(parseInt(number));
+// console.log(parseFloat(number));
 
-// console.log(getMathResult(4, 5))
-
-
-
-
-
-
-// function getMathResult(firstnum, secondnum) {
-//     if(typeof(secondnum) != 'number' || firstnum <= 0){
-//         return firstnum;
-//     }
-//     let str = '';
-//     for (let i = 1; i <= secondnum; i++){
-//         if(secondnum === i){
-//             str += `${firstnum * i}`;   
-//         } else {
-//             str += `${firstnum * i}---`;
-//         }
-//     }
-//     return str;
-// }
-// console.log(getMathResult(1, 5))
-
-
-
-
-
-// Место для третьей задачи
-function getMathResult(num, alt) {
-    if(typeof(alt) != 'number'|| alt <= 0){
-        return num;
-    }
-    let result = '';
-    for(let i = 1; i <= alt; i++){
-        if(alt === i) {
-            result += `${num * i}`;
-        } else {
-              result += `${num * i}---`;
-        }
-    }
-    return result;
-}
-
-console.log(getMathResult(1, 5))
--- js/app.js
+++ js/app.js
@@ -1,83 +1,67 @@
 "use strict";
 
-// const numberOfFilms = prompt('Сколько фильмов вы уже просмотрели?', '');
+let numberOfFilms;
 
-// const personalMoviesDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false
-// }
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+    }
+}
+start();
 
-//  if (personalMoviesDB.count <= 10){
-//     console.log('Просмотрено довольно мало фильмов')
-// } else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
-//     console.log('Вы классический зритель')
-// } else if (personalMoviesDB.count >= 30) {
-//     console.log('Вы киноман');
-// } else {
-//     console.log('error');
-//     i--;
-// }
-//     for(let i = 0; i < 2; i++){
-//         const a = prompt ('Один из последних просмотренных фильмов?', ''),
-//               b = prompt ('На сколько оцените его?', '');
+const personalMoviesDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+function showMyDB (hidden) {
+    if(!hidden) {
+        console.log(personalMoviesDB);
+    }
+}
+showMyDB(personalMoviesDB.privat);
 
+function writeYourGenres () {
+       for(let answ = 1; answ <= 3; answ++){
+    personalMoviesDB.genres[answ - 1] = prompt(`Ваш любимый жанр под номером ${answ}?`);
+  }
+}
+writeYourGenres();
 
-//         if(a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50){
-//         console.log('done');
-//         personalMoviesDB[a] = b;
-
-//      } else {
-//         console.log('error');
-//             i--;
-//      }
-// }
-
-
-
+function rememberMyFilms (){
+    for(let i = 0; i < 2; i++){
+        const a = prompt ('Один из последних просмотренных фильмов?', ''),
+              b = prompt ('На сколько оцените его?', '');
 
 
+        if(a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50){
+        console.log('done');
+        personalMoviesDB[a] = b;
 
-//     console.log(personalMoviesDB);
-
-
-
-// const lines = 8;
-// let result = '';
-
-// for(let index = 0; index < lines; index++) {
-//     if (index == 0) {
-//         result += ' ';
-//     } 
-//     for(let key = 0; key < lines; key++){
-//         result += '#';
-       
-//         for(let step = 0; step < 1; step++){
-//             result += ' ';
-//         }
-//     }
-    
-//     result += ' ';
-  
-//     result += '\n';
-// }
-
-// let num = 10;
-
-// function showFirstMessage(name) {
-//    console.log(name);
-//     let num = 20;
-//     console.log(num)
-// }
-// showFirstMessage('Я люблю Машу');
+     } else {
+        console.log('error');
+            i--;
+     }
+    }
+}
+rememberMyFilms();
 
 
-
-// function calc(a, b) {
-//     return(a * b); 
-// }
-// console.log(calc(1200, 1));
-// console.log(calc(23, 12));
-// console.log(calc(13, 124));
+function detectPersonalLevel () {
+    if (personalMoviesDB.count <= 10){
+        console.log('Просмотрено довольно мало фильмов')
+    } else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+        console.log('Вы классический зритель')
+    } else if (personalMoviesDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+detectPersonalLevel();
+ 
+   
