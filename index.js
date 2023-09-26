f4b9115945bd1a13c54556186c36a9d652fc1134
Alexandr <fresh.fin16@gmail.com>
2023-09-26 20:28:19 +0300

    exercise numbersCoupe

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 "use strict";
+/*Функция принимает только целое число от 1 до 36.
+Если переданный аргумент не число, отрицательное или дробное - возвращается сообщение:
+"Ошибка. Проверьте правильность введенного номера места"
+Если число 0 или больше 36, то сообщение: "Таких мест в вагоне не существует"
+Пример:
+getCoupeNumber(33)  => 9
+getCoupeNumber(7)  => 2
+getCoupeNumber(300)  => "Таких мест в вагоне не существует" */
 
-// const str = 'Some people want to do';
-// console.log(str.slice(5, 11));
-// console.log(str.indexOf('Some'));
-// console.log(str.substring(5, 11));
-// console.log(str.substr(5, 2));
+
+function getCoupeNumber(seatNumber) {
+   
+    if ( typeof(seatNumber) !== 'number' || seatNumber < 0 ||!Number.isInteger(seatNumber)){
+        return 'Ошибка. Проверьте правильность введенного номера места';
+    }            if (seatNumber === 0 || seatNumber > 36 ) { 
+                "Таких мест в вагоне не существует"
+            }
+            return Math.ceil(seatNumber / 4)
+}
+console.log(getCoupeNumber(32));
 
 
-// const num = 12.5;
-// console.log(Math.round(num));
 
-// const number = '12px';
-// // console.log(parseInt(number));
-// console.log(parseFloat(number));
+
+
 
